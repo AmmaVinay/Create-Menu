efc47eeeaac8e05fc730b426e2790249c67c9a11
AmmaVinay <ammavinay917@gmail.com>
2024-04-19 15:20:43 +0530

    first commit

---
+++ create-menu/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CreateMenu from './components/Create-Menu/CreateMenu';
+
+function App() {
+  return (
+    <div className="App">
+      <CreateMenu />
+    </div>
+  );
+}
+
+export default App;
